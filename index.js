30fbbb7fff74f4c68b50c11dc5e7ead85ab006a2
Nic Galluzzo <nicolas@knack.com>
2020-08-14 10:05:39 -0700

    set root path

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html')
+  res.sendFile('index.html', { root: '.' })
 })
 
 app.listen(port, () => {
